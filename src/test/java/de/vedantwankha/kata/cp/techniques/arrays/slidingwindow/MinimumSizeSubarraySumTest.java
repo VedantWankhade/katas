7e37f502af0f7c809535e97af9bc33bc4e610993
VedantWankhade <vedantwankhade17@gmail.com>
2025-03-29 13:35:10 +0530

    Sliding window

---
+++ src/test/java/de/vedantwankha/kata/cp/techniques/arrays/slidingwindow/MinimumSizeSubarraySumTest.java
@@ -0,0 +1,15 @@
+package de.vedantwankha.kata.cp.techniques.arrays.slidingwindow;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinimumSizeSubarraySumTest {
+
+    @Test
+    void minSubArrayLen() {
+        System.out.println(MinimumSizeSubarraySum.minSubArrayLen(7, new int[]{2,3,1,2,4,3}));
+        System.out.println(MinimumSizeSubarraySum.minSubArrayLen(11, new int[]{1,2,3,4,5}));
+        System.out.println(MinimumSizeSubarraySum.minSubArrayLen(0, new int[]{1,1,1,1,1,1,1,1}));
+    }
+}
