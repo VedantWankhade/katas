2dd983cf57bad9d5a961ddfdd078a4facd3fe7ec
Vedant Wankhade <vedantwankhade17@gmail.com>
2025-12-12 02:20:50 +0530

    doc

--- primitive/bit_manipulation/countActiveBits.go
+++ primitive/bit_manipulation/countActiveBits.go
@@ -1,8 +1,20 @@
 package bitmanipulation
 
-// count set bits in possitive num
-//
-// worst time O(n); n is number of bits in num
+/*
+Question:
+
+	Count set bits in positive num
+
+Approach:
+
+	Right shift each bit in num by 1; so that the 1 bit to left of lsb becomes the next lsb,
+	then bitwise and the num with 1.
+	If result is 1 then increment the coundt Repeat till num becomes 1.
+
+Complexity:
+
+	Worst, Average, Best Time: O(n); where n is number of bits in num
+*/
 func CountActiveBits(num uint) uint {
 	var count uint = 0
 	for num > 0 {
--- primitive/bit_manipulation/isLastBitSet.go
+++ primitive/bit_manipulation/isLastBitSet.go
@@ -1,5 +1,18 @@
 package bitmanipulation
 
+/*
+Question:
+
+	Check if the lsb in num is 1
+
+Approach:
+
+	Bitwise and of num with 1 will return 1 if lsb in num is 1
+
+Complexity:
+
+	Worst, Average, Best Time: O(1)
+*/
 func IsLastBitSet(num int) bool {
 	return (num & 1) == 1
 }
